20c4a8666ce1386cf4bd98afedd7bf684dd86883
kleonov <wade.97@mail.ru>
2020-01-24 13:45:44 +0300

    upload

--- src/main/java/com/example/sweater/config/MvcConfig.java
+++ src/main/java/com/example/sweater/config/MvcConfig.java
@@ -1,15 +1,26 @@
 package com.example.sweater.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // этот класс содержит конфигурацию нашего веб-слоя
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Value("${upload.path}")
+    private String uploadPath;
+
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
     }
-    // Метод addViewControllers() (переопределение метода с таким же названием в WebMvcConfigurerAdapter), добавляющий контроллер представления с именеи login
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("C:/Users/Public/Pictures/Sample Pictures/**")
+                .addResourceLocations("file:///" + uploadPath + "/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }
